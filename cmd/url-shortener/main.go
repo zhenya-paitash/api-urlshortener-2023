d705803c7d74832bc36a52062aa6948fcd0781a6
zhenya-paitash <naitavv93@gmail.com>
2023-11-21 17:58:38 +0300

    init
    INIT: prepared the initial directory for the start of the project

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+  // TODO: config
+
+  // TODO: logger
+
+  // TODO: storage
+
+  // TODO: router
+
+  // TODO: run server
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhenya-paitash/api-urlshortener-2023
+
+go 1.21.3
